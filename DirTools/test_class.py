722afeb26fb03becfd379b82d6fd03c802f77456
Niv Sardi <xaiki@evilgiggle.com>
2016-11-02 03:29:46 -0300

    DirTools add simple test
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ DirTools/test_class.py
@@ -0,0 +1,27 @@
+import pytest
+import DirTools
+
+from gi.repository import GLib
+
+ITER_COUNT=10
+
+class TestClass:
+    @pytest.mark.timeout(timeout=3, method='thread')
+    def test_0(self, tmpdir):
+        loop = GLib.MainLoop()
+        self.__called = 0
+
+        def cb_changed(M, p, m, f, o, evt, d=None, e=None):
+            print ('signal', e, p, f, o, evt, d)
+            assert e == 'created'
+            self.__called += 1
+
+        d = tmpdir.mkdir("ndn")
+        m = DirTools.Monitor(str(d))
+        [m.connect(s, cb_changed, s) for s in ['created']]
+        [d.mkdir(str(i)) for i in xrange(ITER_COUNT)]
+
+        GLib.timeout_add_seconds(2, lambda: loop.quit())
+        loop.run()
+        assert self.__called == ITER_COUNT
+
