d4d1429442e9ac6494b80ce2b68d8f28a23548f0
Niv Sardi <xaiki@evilgiggle.com>
2016-11-07 12:30:27 -0300

    simplestore: simple test
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ SimpleStore/test_class.py
@@ -0,0 +1,31 @@
+import pytest
+import time
+
+from os import path, utime
+
+import SimpleStore
+
+def touch(fname, times=None):
+    with open(fname, 'a'):
+        utime(fname, times)
+
+class TestClass:
+    @pytest.fixture
+    def callback(self):
+        return 0
+
+    def test_producer(self, tmpdir):
+        d = tmpdir.mkdir("ndn")
+        p = SimpleStore.Producer()
+        tmpdirpath = str(d.realpath())
+        tmpfilepath = path.join(tmpdirpath, 'test.shard')
+
+        touch(tmpfilepath)
+
+        p.publish_all_names(tmpdirpath)
+        assert p.producers.keys() == [tmpfilepath]
+        assert p.dirs.keys().__len__() == 1
+        p.unpublish(tmpdirpath)
+        assert p.producers.keys() == []
+        assert p.dirs.keys().__len__() == 0
+
