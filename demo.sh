bb4955a10ab14cd2dcf10e9a60380b89b5e24cb5
Niv Sardi <xaiki@evilgiggle.com>
2016-11-15 14:24:01 -0300

    logging for demo.sh
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- demo.sh
+++ demo.sh
@@ -4,9 +4,11 @@ BASE_PATH="/vagrant"
 TEMP_DIR="${BASE_PATH}/tmp"
 APPIDS="10521bb3a18b573f088f84e59c9bbb6c2e2a1a67"
 
-run_router="printf '\033]2;%s\033\\' 'router'; python ${BASE_PATH}/edge-router-avahi.py"
-run_store="printf '\033]2;%s\033\\' 'store'; python ${BASE_PATH}/ostree-store.py -r repo -t ${TEMP_DIR}"
-run_dbus_consumer="printf '\033]2;%s\033\\' 'dbus'; python ${BASE_PATH}/simulate-dbus-consumer.py $APPIDS"
+title="printf '\033]2;%s\033\\'"
+
+run_router="$title 'router'; python ${BASE_PATH}/edge-router-avahi.py 2>&1 | tee ${BASE_PATH}/router.log"
+run_store="$title 'store'; python ${BASE_PATH}/ostree-store.py -r repo -t ${TEMP_DIR} 2>&1 | tee ${BASE_PATH}/store.log"
+run_dbus_consumer="$title 'dbus'; python ${BASE_PATH}/simulate-dbus-consumer.py $APPIDS 2>&1 | tee ${BASE_PATH}/dbus.log"
 
 nfd-stop
 rm -rf ${TEMP_DIR}/*
