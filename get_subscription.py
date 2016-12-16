61229d7dedb79a5171e88046d73843e72303a464
Niv Sardi <xaiki@evilgiggle.com>
2016-12-16 09:07:48 -0300

    get_subscription: auto-select usb drive if plugged
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- get_subscription.py
+++ get_subscription.py
@@ -1,8 +1,10 @@
 import logging
 import argparse
+from os import path
 
 from gi.repository import GLib
 
+from eos_data_distribution.defaults import ENDLESS_NDN_CACHE_PATH
 from eos_data_distribution.subscription import Fetcher
 from eos_data_distribution.parallel import Batch
 from eos_data_distribution.ndn.base import GLibUnixFace
@@ -13,8 +15,22 @@ logger = logging.getLogger(__name__)
 loop = GLib.MainLoop()
 face = GLibUnixFace()
 
+def mount_get_root (mount):
+    drive = mount.get_drive()
+    root = mount.get_root()
+
+    print "found drive", drive.get_name()
+    return path.join (root.get_path(), ENDLESS_NDN_CACHE_PATH)
+
+monitor = Gio.VolumeMonitor.get()
+usb_stores = [mount_get_root(mount) for mount in monitor.get_mounts()]
+try:
+    store_dir = usb_stores[0]
+except:
+    store_dir = "./eos_subscription_data"
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--store-dir", default="./eos_subscription_data")
+parser.add_argument("-t", "--store-dir", default=store_dir)
 parser.add_argument("appids", nargs='+')
 
 args = parser.parse_args()
