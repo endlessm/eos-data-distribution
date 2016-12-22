641aeddf1e275b5a367b87e90c7a8d2d48e451c0
Niv Sardi <xaiki@evilgiggle.com>
2016-12-22 11:44:11 -0300

    http: add posibility to establish a consumer in module test
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- eos_data_distribution/ndn/http.py
+++ eos_data_distribution/ndn/http.py
@@ -103,6 +103,7 @@ class Producer(chunks.Producer):
 
 
 if __name__ == '__main__':
+    from . import file
     from gi.repository import GLib
     import re
 
@@ -110,6 +111,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--name")
+    parser.add_argument("-o", "--output")
     parser.add_argument("url")
 
     args = parser.parse_args()
@@ -118,6 +120,10 @@ if __name__ == '__main__':
     else:
         name = re.sub('https?://', '', args.url)
 
+    loop = GLib.MainLoop()
     producer = Producer(name, args.url, auto=True)
+    if args.output:
+        consumer = file.FileConsumer(name, filename=args.output, auto=True)
+        consumer.connect('complete', lambda  *a: loop.quit())
 
-    GLib.MainLoop().run()
+    loop.run()
