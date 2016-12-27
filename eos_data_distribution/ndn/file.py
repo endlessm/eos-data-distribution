4bc3abf8341b61f7e9e56c4062b279c6d91c332c
Niv Sardi <xaiki@evilgiggle.com>
2016-12-27 18:47:17 -0300

    ndn: rename produce/consume to start()
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- eos_data_distribution/ndn/file.py
+++ eos_data_distribution/ndn/file.py
@@ -107,7 +107,7 @@ class FileConsumer(chunks.Consumer):
         super(FileConsumer, self).__init__(name, *args, **kwargs)
 
     # XXX: This is disgusting hackery. We need to remove auto=True.
-    def consume(self):
+    def start(self):
         # If we have an existing download to resume, use that. Otherwise,
         # request the first segment to bootstrap us.
         try:
