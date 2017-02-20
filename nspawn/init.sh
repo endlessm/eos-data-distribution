cd8df3540a8e063a941d8c0163f7edc5692d2f27
Niv Sardi <xaiki@evilgiggle.com>
2017-02-20 11:50:18 -0300

    nspawn: dump all in srcdir
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- nspawn/init.sh
+++ nspawn/init.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 # Init OSTree Repo
 
-mkdir -p repo
-ostree --repo=repo init --mode=bare
-ostree --repo=repo remote add --no-gpg-verify endless https://endless:kassyicNigNarHon@origin.ostree.endlessm.com/staging/dev/eos-amd64
-sudo ostree --repo=repo pull endless master/amd64
-sudo ostree --repo=repo checkout endless:master/amd64 checkout
+srcdir=`realpath $0`
+srcdir=`dirname ${srcdir}`
+repodir=${srcdir}/repo
+checkoutdir=${srcdir}/checkout
+
+mkdir -p ${repodir}
+ostree --repo=${repodir} init --mode=bare
+ostree --repo=${repodir} remote add --no-gpg-verify endless https://endless:kassyicNigNarHon@origin.ostree.endlessm.com/staging/dev/eos-amd64
+sudo ostree --repo=${repodir} pull endless master/amd64
+sudo ostree --repo=${repodir} checkout endless:master/amd64 ${checkoutdir}
--- nspawn/start.sh
+++ nspawn/start.sh
@@ -4,11 +4,17 @@ set -e
 set -x
 name=$1
 
+srcdir=`realpath $0`
+srcdir=`dirname ${srcdir}`
+basedir=${srcdir}/${name}
+
 test -n "${name}" || (echo "No name provided: Usage $0 name"; exit 1);
 
-trap "sudo umount ${name}/checkout" EXIT
+trap "umount ${basedir}/checkout/etc && umount ${basedir}/checkout" EXIT
+shift
 
-for d in up work checkout; do mkdir -p ${name}/${d}; done
-sudo mount -t overlay overlay -olowerdir=checkout,upperdir=${name}/up,workdir=${name}/work ${name}/checkout
-sudo systemd-nspawn -D ${name}/checkout --network-zone=ndn -n --machine=${name}-nspawn --bind $PWD/checkout/usr/etc:/etc --bind $PWD/system:/etc/systemd/system
+for d in up work checkout; do mkdir -p ${basedir}/${d}; done
+mount -t overlay overlay -olowerdir=checkout,upperdir=${basedir}/up,workdir=${basedir}/work ${basedir}/checkout
+mount -o bind ${basedir}/checkout/usr/etc ${basedir}/checkout/etc
+systemd-nspawn -D ${basedir}/checkout --network-zone=ndn -n --machine=${name} --bind $PWD/system:/etc/systemd/system $@
 
