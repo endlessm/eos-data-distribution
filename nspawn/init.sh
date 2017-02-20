19084936628ff6f5cb4f9ccbdfcbd4e36ba2e985
Niv Sardi <xaiki@evilgiggle.com>
2017-02-20 12:24:32 -0300

    nspawn: tools need to be run as root, don't sudo inside
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- nspawn/init.sh
+++ nspawn/init.sh
@@ -9,5 +9,5 @@ checkoutdir=${srcdir}/checkout
 mkdir -p ${repodir}
 ostree --repo=${repodir} init --mode=bare
 ostree --repo=${repodir} remote add --no-gpg-verify endless https://endless:kassyicNigNarHon@origin.ostree.endlessm.com/staging/dev/eos-amd64
-sudo ostree --repo=${repodir} pull endless master/amd64
-sudo ostree --repo=${repodir} checkout endless:master/amd64 ${checkoutdir}
+ostree --repo=${repodir} pull endless master/amd64
+ostree --repo=${repodir} checkout endless:master/amd64 ${checkoutdir}
