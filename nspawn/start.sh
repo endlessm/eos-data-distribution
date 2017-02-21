6db256050a6845c9a2a04d4c213fdae8052e09e3
Niv Sardi <xaiki@evilgiggle.com>
2017-02-21 10:31:16 -0300

    nspawn: mount everything in srcdir
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- nspawn/start.sh
+++ nspawn/start.sh
@@ -10,11 +10,13 @@ basedir=${srcdir}/${name}
 
 test -n "${name}" || (echo "No name provided: Usage $0 name"; exit 1);
 
-trap "umount ${basedir}/checkout/etc && umount ${basedir}/checkout" EXIT
+trap "umount ${basedir}/checkout/etc; umount ${basedir}/checkout" EXIT
 shift
 
 for d in up work checkout; do mkdir -p ${basedir}/${d}; done
-mount -t overlay overlay -olowerdir=checkout,upperdir=${basedir}/up,workdir=${basedir}/work ${basedir}/checkout
+mkdir -p ${srcdir}/checkout/etc
+mount -t overlay overlay -olowerdir=${srcdir}/checkout,upperdir=${basedir}/up,workdir=${basedir}/work ${basedir}/checkout
 mount -o bind ${basedir}/checkout/usr/etc ${basedir}/checkout/etc
-systemd-nspawn -D ${basedir}/checkout --network-zone=ndn -n --machine=${name} --bind $PWD/system:/etc/systemd/system $@
+echo ${name} > ${basedir}/checkout/etc/hostname
+systemd-nspawn -D ${basedir}/checkout --network-zone=ndn -n --machine=${name} --bind ${srcdir}/system:/etc/systemd/system $@
 
