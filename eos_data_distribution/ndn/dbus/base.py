da8a6700e4186caf11520c05ee00274ec1b90f56
Cosimo Cecchi <cosimoc@gnome.org>
2017-05-01 14:51:26 -0700

    dbus/base: interest must be a string
    Not a Name, so use str(name) in this case.

--- eos_data_distribution/ndn/dbus/base.py
+++ eos_data_distribution/ndn/dbus/base.py
@@ -122,7 +122,7 @@ class Consumer(Base):
 
     def expressInterest(self, interest=None, try_again=False):
         if not interest:
-            interest = self.name
+            interest = str(self.name)
 
         self.interest = interest
         if not self.con:
