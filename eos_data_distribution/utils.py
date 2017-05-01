084ba738d50abd577507699a1675c6227f3496d9
Cosimo Cecchi <cosimoc@gnome.org>
2017-05-01 13:00:17 -0700

    soma_producer: properly support verbosity argument
    We will share this code with the other callers too.

--- eos_data_distribution/utils.py
+++ eos_data_distribution/utils.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python
 # -*- Mode:python; coding: utf-8; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
 #
-# Copyright (C) 2016 Endless Mobile, Inc.
-# Author: Niv Sardi <xaiki@endlessm.com>
+# Copyright (C) 2017 Endless Mobile, Inc.
+# Author: Cosimo Cecchi <cosimo@endlessm.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -18,21 +17,26 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # A copy of the GNU Lesser General Public License is in the file COPYING.
 
-import gi
-gi.require_version('GLib', '2.0')
+import argparse
+import logging
 
-from gi.repository import GLib
+def parse_args(parser=None, include_name=True):
+    if parser is None:
+        parser = argparse.ArgumentParser()
 
-from eos_data_distribution import utils
-from eos_data_distribution.soma_subscription_fetcher import Fetcher
+    if include_name:
+        parser.add_argument('--name', '-n',
+                            help='name of the requested interest')
+    parser.add_argument('-v', action='count',
+                        help='verbosity level')
+    args = parser.parse_args()
 
+    if args.v > 1:
+        logging.basicConfig(level=logging.DEBUG)
+    elif args.v == 1:
+        logging.basicConfig(level=logging.INFO)
+    else:
+        # We use the default WARNING level if -v was not specified
+        pass
 
-def main():
-    utils.parse_args()
-    fetcher = Fetcher()
-    loop = GLib.MainLoop()
-    loop.run()
-
-
-if __name__ == "__main__":
-    main()
+    return args
